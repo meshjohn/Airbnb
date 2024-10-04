e33c79034e7461b74f47cf9a6b757d5cacb38de9
meshJohn <meshjohn1@gmail.com>
2024-10-04 10:34:55 +0300

    cros origin error

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -30,7 +30,7 @@ export async function UserNav() {
           <img
             src={
               user?.picture ??
-              "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
+              "/default-avatar-icon-of-social-media-user-vector.jpg"
             }
             alt="image of the user"
             className="rounded-full h-8 w-8 hidden lg:block"
